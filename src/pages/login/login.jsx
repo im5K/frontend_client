741c10087522bc88511cae4488f95a703b57eddd
zth.laptop <thzhao@mail.ustc.edu.cn>
2020-08-22 16:19:06 +0800

    p11

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,25 @@
 import React from "react"
 //后台管理
+import './login.less'
+import logo from "./images/logo.png"
+
 export default class Login extends React.Component{
    
     render(){
         return(
-            <div>Login</div>
+          <div className="login">
+              <header className="login-header">
+                  <img src={logo} alt="logo"/>
+                  <h1>后台管理系统</h1>
+
+              </header>
+              <section className="login-content">
+              <h3>用户登录</h3>
+              <div>dasdadsad</div>
+              </section>
+             
+              
+          </div>
         )
 
     }
